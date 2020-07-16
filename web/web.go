d044867284399f0baf2fbdfc178f5b8391c973f7
Alec Scott <alecbcs@gmail.com>
2020-07-16 11:27:47 -0700

    Fix Templates being inifite number of pages

--- web/web.go
+++ web/web.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -24,6 +26,21 @@ func init() {
 // Page is the default web handler to render the "arkstat" html page.
 func Page(w http.ResponseWriter, r *http.Request) {
 	var t *template.Template
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+	// Return a 404 if the template doesn't exist
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+	// Return a 404 if the request is for a directory
+	if info.IsDir() && fp != "templates" {
+		http.NotFound(w, r)
+		return
+	}
+
 	t = template.New("index.html")
 
 	t, _ = t.ParseFiles("templates/index.html")
