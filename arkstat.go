2e9f28a197efa64116a93727ca03c0804d3ccb4e
Alec Scott <alecbcs@gmail.com>
2020-08-16 13:58:25 -0700

    Added Favicons

--- arkstat.go
+++ arkstat.go
@@ -16,6 +16,7 @@ func main() {
 	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("templates/fonts"))))
 	http.Handle("/graphic/", http.StripPrefix("/graphic/", http.FileServer(http.Dir("templates/graphic"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("templates/images"))))
+	http.Handle("/favicon.ico", http.FileServer(http.Dir("templates/images")))
 
 	http.HandleFunc("/beacon", web.Beacon)
 	http.HandleFunc("/", web.Page)
